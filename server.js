65f87c6f9fa7a6fca4c38b1e1e52bab465a70bf9
Prateek Madapurmath <prateekm33@gmail.com>
2016-06-11 14:22:51 -0700

    testing

--- server.js
+++ server.js
@@ -131,9 +131,9 @@ app.post('/roam', function(req, res) {
   console.log('about to query db');
 
   db.cypherAsync({query: 'MATCH (n:User) WHERE n.email={email} return n', params: {email: userEmail}}).then(result => {
-    var status = result[0];
+    var status = result[0]['n'];
     console.log('statussss', status.properties);
-    if (status['n'].properties.status === 'INACTIVE') {
+    if (status.properties.status === 'INACTIVE') {
       console.log('inactive status');
         //TODO: first do query similar to line 198, then send back the response as roaminfo
         //TODO: change to {info: roaminfo, message: 'matched'}
